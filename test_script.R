8f17554ef62dab8f5b43e62eeb1a6dc3d6b1e40e
Štěpán Jabůrek <168758805+stepanjaburek@users.noreply.github.com>
2026-03-18 20:26:49 +0100

    Update test_script.R

--- test_script.R
+++ test_script.R
@@ -1,7 +1,11 @@
 # endogenous group confound example
+#install.packages("remotes")
+remotes::install_github("stepanjaburek/bayeswatch")
 library(bayeswatch)
 library(brms)
-set.seed(8672)
+
+
+set.seed(11)
 
 N_groups <- 30
 N_id <- 200
@@ -13,36 +17,15 @@ X <- rnorm(N_id, Ug[g])                               # individual varying trait
 Z <- rnorm(N_groups)                                   # group varying trait (observed)
 Y <- rbinom(N_id, size = 1, prob = plogis(a0 + X + Ug[g] + bZY * Z[g]))
 
-# Map the group-level Z to the individuals in g
+
 data <- data.frame(
   Y = Y,
   X = X,
-  Z = Z[g],  # expands Z from 30 values to 200 based on group index
+  Z = Z[g],  
   g = g
 )
 
-# Run brms with the HMC animation in the Viewer pane
+
 mod <- brm_with_viz(Y ~ X + Z + (1 | g), data = data, family = bernoulli(),
                     chains = 4, cores = 4)
 summary(mod)
-dat <- list(Y=Y,X=X,g=g,Ng=N_groups,Z=Z)
-# naive model
-m0 <- ulam_with_viz(
-    alist(
-        Y ~ bernoulli(p),
-        logit(p) <- a + bxy*X + bzy*Z[g],
-        a ~ dnorm(0,10),
-        c(bxy,bzy) ~ dnorm(0,1)
-    ) , data=dat , chains=4 , cores=4 )
-precis(m0)
-
-
-
-
-m0 <- ulam_with_viz(
-+     alist(
-+         Y ~ bernoulli(p),
-+         logit(p) <- a + bxy*X + bzy*Z[g],
-+         a ~ dnorm(0,10),
-+         c(bxy,bzy) ~ dnorm(0,1)
-+     ) , data=dat , chains=4 , cores=4 )
